6901983c7b12e2a4cd2b2ffd466bd5db74157f7d
joaquin lopez <joakomask@gmail.com>
2023-05-26 09:14:16 -0400

    primer commit fixed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import express from "express"
+import dotenv from "dotenv"
 import router from "./routes/routes.js"
-const PORT =3000
+dotenv.config()
+const PORT =process.env.PORT || 3000
 const app = express()
 app.use(express.json())
 app.use("/whatsapp", router)
 app.listen(PORT, () => {
-    console.log('app running on port http://localhost:3000/')
+    console.log(`app running on port http://localhost:${PORT}/:`)
 })
