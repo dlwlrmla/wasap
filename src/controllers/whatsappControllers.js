aaf47ad1cd960baec47bffad516c102f600e4b45
joaquin lopez <joakomask@gmail.com>
2023-05-26 09:24:38 -0400

    a1

--- src/controllers/whatsappControllers.js
+++ src/controllers/whatsappControllers.js
@@ -5,7 +5,7 @@ export const verifyToken = (req,res ) => {
     try {
         let accessToken = process.env.ACCESSTOKEN
         let token = req.query["hub.verify_token"]
-        let challenge = req.body["hub.challenge"]
+        let challenge = req.query["hub.challenge"]
 
         if(challenge != null && token==accessToken){
             res.send(challenge)
